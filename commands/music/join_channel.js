8d4d7858664661989807ecfa1720b3443b69f189
magz1k <46112988+magz1k@users.noreply.github.com>
2018-12-25 00:29:03 +0300

    Create join_channel.js

---
+++ commands/music/join_channel.js
@@ -0,0 +1,59 @@
+const commando = require('discord.js-commando');
+const YTDL = require('ytdl-core');
+
+function Play(connection, message)
+{
+    var server = servers[message.guild.id];
+    server.dipatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dipatcher.on("end", function(){
+        if(server.queue[0])
+        {
+            Play(connection,message);
+        }
+        else 
+        {
+            connection.disconnect();
+        }
+    });
+}
+
+class JoinChannelCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'join',
+            group: 'music',
+            memberName: 'join',
+            description: 'Подключение бота к голосовому каналу'
+        });
+    }
+
+    async run(message, args)
+    {
+        if(message.member.voiceChannel)
+        {
+            if(!message.guild.voiceConnection)
+            {
+                if(!servers[message.guild.id])
+                {
+                    servers[message.guild.id] = {queue: []}
+                }
+                message.member.voiceChannel.join()
+                    .then(connection =>{
+                        var server =  servers[message.guild.id];
+                        message.reply(" Ты звал меня ?");
+                        server.queue.push(args);
+                        Play(connection, message);
+                    })
+            }
+        }
+        else
+        {
+            message.reply("Присоединись к каналу, что позвать бота");
+        }
+    }
+}
+
+module.exports = JoinChannelCommand;
