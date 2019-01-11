30ce78b455a939910a79b04314e5c18f9d6f73a3
magz1k <46112988+magz1k@users.noreply.github.com>
2019-01-11 14:27:50 +0300

    Create monkmw.js

---
+++ commands/class/monkmw.js
@@ -0,0 +1,31 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class MonkmwCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'monk',
+            group: 'class',
+            memberName: 'monk',
+            description: 'класс Monk'
+        });
+    }
+
+    async run(message , args)
+    {
+        var myinfo = new discord.RichEmbed()
+            .setTitle("Монк")
+            .addField("**Заголовок 1**", "*какой-то текст или ссылка*", true)
+            .addField("**Заголовок 1**", "*какой-то текст или ссылка*", true)
+            .addField("**Заголовок 1**", "**какой-то текст или ссылка*", true)
+            .setDescription ("- Ткач туманов")
+            .setColor("#03ff8b")
+            .setThumbnail("https://d1u5p3l4wpay3k.cloudfront.net/wowpedia/2/24/Ui-charactercreate-classes_monk.png")
+            .setFooter("(c) Smerch Bot")
+
+            message.channel.sendEmbed(myinfo);
+    }
+}
+module.exports = MonkmwCommand;
