6f350a5c913a5e309f6073105c441db584c62621
magz1k <46112988+magz1k@users.noreply.github.com>
2019-01-11 13:12:15 +0300

    Update monk.js

--- commands/simple/monk.js
+++ commands/simple/monk.js
@@ -8,7 +8,7 @@ class MonkCommand extends commando.Command
         super(client,{
             name: 'monk',
             group: 'simple',
-            memberName: 'Monk WA',
+            memberName: 'monk',
             description: 'WeakAurus - Monk'
         });
     }
