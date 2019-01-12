10fc7ec2ad184b4d2d3333dc3128251863cdf7a2
magz1k <46112988+magz1k@users.noreply.github.com>
2019-01-12 18:55:01 +0300

    Create monkmw.js

---
+++ commands/class/monkmw.js
@@ -0,0 +1,38 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class MonkMWCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'monkmw',
+            group: 'class',
+            memberName: 'monkmw',
+            description: 'Монах Ткач туманов'
+        });
+    }
+
+    async run(message , args)
+    {
+        var myinfo = new discord.RichEmbed()
+                .setAuthor(`Hibiki | 響`, client.user.avatarURL)
+                .addField(`Version`, `1.0`, true)
+                .addField(`Node JS`, `8.11.3`, true)
+                .addField(`Library`, `[discord.js](https://discord.js.org/#/)`, true)
+                .addField(`Uptime`, `${uptime}`, true)
+                .addField(`Servers`, `${client.guilds.size}`, true)
+                .addField(`Users`, `${client.users.size}`, true)
+                .addField(`Website`, `[hibikimoe.xyz](https://hibikimoe.xyz/)`, true)
+                .addField(`Discord`, `[hibikimoe.xyz/discord](https://discord.gg/NNwzMm)`, true)
+                .addField(`Invite`, `[Not available yet](https://discord.gg/NNwzMm)`, true)
+                .addField(`Developer`, `Steve2312#0431`, true)
+                .setFooter("Prefix: ! | This bot is still under construction", "https://a.ppy.sh/5414370_1530431526.jpeg")
+            .setColor("#03ff8b")
+            .setThumbnail("https://d1u5p3l4wpay3k.cloudfront.net/wowpedia/2/24/Ui-charactercreate-classes_monk.png")
+            .setFooter("(c) Smerch Bot")
+
+            message.channel.sendEmbed(myinfo);
+    }
+}
+module.exports = MonkMWCommand;
