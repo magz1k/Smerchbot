b2b5a736ced45bd8261b7dfb3a0ddde631ded5c0
magz1k <46112988+magz1k@users.noreply.github.com>
2019-01-13 01:11:14 +0300

    Update monkwm.js

--- commands/spec/monkwm.js
+++ commands/spec/monkwm.js
@@ -17,10 +17,10 @@ class MonkwmCommand extends commando.Command
     {
         var myinfo = new discord.RichEmbed()
             .setTitle("Монк - Ткач туманов")
-            .addField("**WeakAurus**", "[Battle for Azeroth] (https://wago.io/N173IReX7)", true)
-            .addField("**WeakAurus 2 **", "[masked links](http://google.com) inside of rich embeds.", true)
+            .addField("**WeakAurus**", "[Download](http://wago.io/N173IReX7) support BFA.", true)
+            .addField("**TellMeWhen**", "[Download](http://wago.io/N173IReX7) support BFA.", true)
             .addField("**WeakAurus 3 **", "[Battle for Azeroth] (https://wago.io/N173IReX7)", true)
-            .setImage("https://cdn.discordapp.com/attachments/519132230010929153/533674482049613825/wa.png")
+            .setImage("https://cdn.discordapp.com/attachments/519132230010929153/533769396074840085/wa2.png")
             .setDescription ("Addons")
             .setColor("#03ff8b")
             .setThumbnail("https://d1u5p3l4wpay3k.cloudfront.net/wowpedia/2/24/Ui-charactercreate-classes_monk.png")
