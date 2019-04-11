2c5b596f0ae1dd769d84c357e026f2256437f705
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-12 01:01:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 const bot = new Commando.Client();
 
 bot.on("ready", () => {
-    client.user.setActivity("my code", { type: "WATCHING"})
+    bot.user.setActivity("my code", { type: "WATCHING"})
 })
 
 bot.registry.registerGroup('simple', 'Simple');
