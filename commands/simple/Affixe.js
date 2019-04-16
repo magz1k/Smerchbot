117d2704627b590c5535de5a48133ec101966e7e
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 01:11:48 +0300

    Create Affixe.js

---
+++ commands/simple/Affixe.js
@@ -0,0 +1,31 @@
+const commando = require('discord.js-commando');
+
+class AffCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'aff',
+            group: 'simple',
+            memberName: 'aff',
+            description: 'affixes'
+        });
+    }
+
+    async run(message, args)
+    {
+     const request = require('request-promise')
+
+module.exports = ({ character, realm, region }) => {
+  return request.get(`https://raider.io/api/v1/mythic-plus/affixes?region=${region}`)
+    .then(res => {
+      const options = {}
+      const data = JSON.parse(res)
+      const message = data.title
+
+      return [message, options]
+    })
+}
+}
+
+module.exports = AffCommand;
