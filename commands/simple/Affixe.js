3f4747fa098134bef24f7c8da092dae515d8bb76
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 01:19:25 +0300

    Update Affixe.js

--- commands/simple/Affixe.js
+++ commands/simple/Affixe.js
@@ -12,16 +12,17 @@ class AffCommand extends commando.Command
         });
     }
 
-    async run(message, args)
-    {
+const request = require('request-promise')
+
 module.exports = ({ character, realm, region }) => {
   return request.get(`https://raider.io/api/v1/mythic-plus/affixes?region=${region}`)
     .then(res => {
       const options = {}
       const data = JSON.parse(res)
       const message = data.title
+
       return [message, options]
-    }
+    })
 }
 
 module.exports = AffCommand;
