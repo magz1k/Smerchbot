22ee749def48052ec9ff72862113738416f34805
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 01:26:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,59 @@ bot.on ('message', function(message){
     let answers = ["Эти древние друиды (жарг. медведи) поклоняются тотему медведа и используют его силу для защиты их сообщества от внешней угрозы. Они любят принимать форму медведя и шататься по диким местам Калимдора. Если их взбесить, они могут показать очень свирепую силу и стойкость. Их магические умения увеличивают боевые навыки и храбрость их союзников.", "Уж и друид теперь не тот,  Он больно стал неполноценным.  Медведь, совух и даже кот В тоске приходят неизменной.", "Друид Страж - (танк, медведь)", "ахахах ты серьезно?))))"]; //массив ответов
     let rand = Math.floor(Math.random()*answers.length); //получаем случайное число от 0 до `кол-ва ответов`
     message.channel.send(answers[rand]);
-    }
+    }	
+    else if (input === "!AFF") {
+		var options = {
+	    uri: 'https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en',
+	    json: true // Automatically parses the JSON string in the response
+		};
+		rp(options)
+    .then(function (response) {
+			if (response.affix_details[2].name === "Tyrannical") {
+				var thumbnail_url = "https://wow.zamimg.com/images/wow/icons/large/achievement_boss_archaedas.jpg";
+			} else {
+				var thumbnail_url = "https://wow.zamimg.com/images/wow/icons/large/ability_toughness.jpg";
+			}
+			message.channel.send({embed: {
+				color: 10691119,
+				author: {
+					name: "Affixes",
+					url: response.leaderboard_url
+				},
+				fields: [
+					{
+						name: response.affix_details[0].name,
+						value: response.affix_details[0].description
+					},
+					{
+						name: response.affix_details[1].name,
+						value: response.affix_details[1].description
+					},
+					{
+						name: response.affix_details[2].name,
+						value: response.affix_details[2].description
+					}
+				],
+				timestamp: new Date(),
+				footer: {
+					icon_url: "https://s3.amazonaws.com/reamaze-prod/avatars/8268745/thumb/raiderio_square_bg.jpg?1503530714",
+					text: 'Pulled from Raider.IO'
+				},
+				thumbnail: {
+      		"url": thumbnail_url
+    		}
+			}});
+    })
+    .catch(function (err) {
+			console.log(err);
+			var affix = affixes.get_affixes();
+			try {
+				message.channel.send(affix[0] + affix[1] + "\nFor more check out: <https://mythicpl.us/>");
+			} catch (err) {
+				message.channel.send("Weekly Affixes: <https://mythicpl.us/> \n");
+			}
+    });
+	}
 });
 
 var badWords = [
