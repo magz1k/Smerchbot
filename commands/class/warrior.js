66a02a26bd9a5aa677299c55378060daca2f311f
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 14:37:47 +0300

    Create warrior.js

---
+++ commands/class/warrior.js
@@ -0,0 +1,24 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js')
+
+class WarriorCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'warrior',
+            group: 'class',
+            memberName: 'warrior',
+            description: 'ВОИН'
+        });
+    }
+
+    async run(message , args)
+    {
+        var myInfo = new.discord.RichEmbed()
+        .setDescription("hello")
+        .setColor("#fff000")
+        message.channel.sendEmbed(myInfo);
+    }
+}
+module.exports = WarriorCommand;
