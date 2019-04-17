3b5ab1e00b0bd3757e6d3f1bd1c2d045e2b0fb7f
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 11:24:13 +0300

    Update rouge.js

--- commands/class/rouge.js
+++ commands/class/rouge.js
@@ -15,53 +15,9 @@ class RougeCommand extends commando.Command
 
     async run(message , args)
     {
-        const embed = {
-  "title": "title ~~(did you know you can have markdown here too?)~~",
-  "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
-  "url": "https://discordapp.com",
-  "color": 10003140,
-  "timestamp": "2019-04-17T08:14:50.077Z",
-  "footer": {
-    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-    "text": "footer text"
-  },
-  "thumbnail": {
-    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-  },
-  "image": {
-    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-  },
-  "author": {
-    "name": "author name",
-    "url": "https://discordapp.com",
-    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-  },
-  "fields": [
-    {
-      "name": "🤔",
-      "value": "some of these properties have certain limits..."
-    },
-    {
-      "name": "😱",
-      "value": "try exceeding some of them!"
-    },
-    {
-      "name": "🙄",
-      "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
-    },
-    {
-      "name": "<:thonkang:219069250692841473>",
-      "value": "these last two",
-      "inline": true
-    },
-    {
-      "name": "<:thonkang:219069250692841473>",
-      "value": "are inline fields",
-      "inline": true
-    }
-  ]
-};
-channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
-    }
+        var myinfo = new discord.RichEmbed()
+            .setDescription ("Выберите спек")
+            .setColor("#000000")
+    }  
 }
 module.exports = RougeCommand;
