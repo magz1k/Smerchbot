ef09733527059643c4aff45a874a5fb7b170438f
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 11:10:18 +0300

    Create rouge.js

---
+++ commands/class/rouge.js
@@ -0,0 +1,32 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class DruidCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'rouge', 
+            group: 'class',
+            memberName: 'rouge', 
+            description: 'класс Rouge' 
+        });
+    }
+
+    async run(message , args)
+    {
+        var myinfo = new discord.RichEmbed()
+            .setTitle("Класс Rouge")
+            .addField("**Assassination**", "[ссылка на гайд на англ](https://www.icy-veins.com/wow/guardian-druid-pve-tank-guide)", true) 
+            .addField("**Outlaw**", "*[ссылка на гайд на англ](https://www.icy-veins.com/wow/guardian-druid-pve-tank-guide)*", false)
+            .addField("**Subtlety**", "*[ссылка на гайд на англ](https://www.icy-veins.com/wow/guardian-druid-pve-tank-guide)*", false) 
+            .addField("**Leveling Up (1-120)**", "[ссылка на гайд](https://www.icy-veins.com/wow/guardian-druid-pve-tank-guide)", true) 
+            .setDescription ("Выберите спек")
+            .setColor("#03ff8b") ///тут ненадо
+            .setThumbnail("https://media.discordapp.net/attachments/541959166185701396/567983478717546506/9_2small_.png") /// тут ненадо
+            .setFooter("(c) Smerch Bot")
+
+            message.channel.sendEmbed(myinfo);
+    }
+}
+module.exports = DruidCommand;
